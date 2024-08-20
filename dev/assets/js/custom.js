45cc33ce8bea65a258dba69f5d0c53df40952b6f
unknown <kateryna.yakimuk@gmail.com>
2024-08-20 12:56:11 +0300

    header mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,15 @@
-  
+  $(document).ready(function(){
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+      }); 
+  })
+
+  const burger = document.querySelector('.hamburger')
+  const headerNav = document.querySelector('.header__nav')
+
+  function showBurgerMenu(){
+    burger.classList.toggle('is-active');
+    headerNav.classList.toggle('show');
+  }
+
+  burger.addEventListener('click', showBurgerMenu)
