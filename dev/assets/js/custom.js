3a069e51e79a44f675decade535521abd83394f3
unknown <kateryna.yakimuk@gmail.com>
2024-08-22 23:29:19 +0300

    first-screen desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -5,11 +5,41 @@
   })
 
   const burger = document.querySelector('.hamburger')
+  const body = document.querySelector('body')
   const headerNav = document.querySelector('.header__nav')
 
   function showBurgerMenu(){
-    burger.classList.toggle('is-active');
     headerNav.classList.toggle('show');
+    burger.classList.toggle('is-active');
+    body.classList.toggle('overflow-hidden');
   }
 
   burger.addEventListener('click', showBurgerMenu)
+
+   
+  const swiper = new Swiper('.slider-info', {  
+    spaceBetween: 40,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    autoplay: false,
+
+    navigation:  {
+      enabled: false,
+    },
+
+    breakpoints: {
+      1024: {
+        spaceBetween: 40,
+        navigation: {
+          enabled: true,
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      }
+    }
+    
+});
